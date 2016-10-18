1940c654519f4badf3f37e51da96dd70962e9868
joseprtv <joseprtv@hotmail.com>
2016-10-18 15:47:47 -0500

    Revert "Inicial"
    This reverts commit c13c06766f7e31149622a49c7de9630730733486.
    
    # Conflicts:
    #	New Unity Project/Library/CurrentLayout.dwlt
    #	New Unity Project/Library/InspectorExpandedItems.asset
    #	New Unity Project/Library/LastSceneManagerSetup.txt
    #	New Unity Project/Library/assetDatabase3
    #	New Unity Project/Library/expandedItems
    #	New Unity Project/Library/metadata/00/00000000000000003000000000000000
    #	New Unity Project/Library/metadata/00/00000000000000004000000000000000
    #	New Unity Project/Library/metadata/00/00000000000000006100000000000000
    #	New Unity Project/Library/shadercompiler-UnityShaderCompiler.exe0.log
    #	New Unity Project/ProjectSettings/TagManager.asset

---
+++ Assets/Municion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Municion : MonoBehaviour {
+    public int municionPizza;
+    static public int municionPizza2;
+	// Use this for initialization
+	void Start ()
+    {
+        //Asignar el valor inicial de municion por cada nivel a municionPizza2, municionPizza es el dato que va a variar y modificar la cantidad de pizza.
+        municionPizza = municionPizza2;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (disparos == true)
+        {
+            municionPizza--;
+        }
+	}
+}
