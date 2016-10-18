a5a6af5cf3636165c0dda4358b1ea7c74ad2e9bb
JuviJew777 <poire.ortiz.david@gmail.com>
2016-10-18 15:37:08 -0500

    inicial

---
+++ New Unity Project/Assets/ChoqueyMovimiento.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChoqueyMovimiento : MonoBehaviour {
+    Vector2 moveDirection = Vector2.zero;
+    public Transform enemigo;
+    public GameObject enemigo2;
+    public bool choque = false;
+    public float velociPuti;
+    // Use this for initialization
+    void Start () {
+        moveDirection = new Vector2(velociPuti,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        enemigo.position = (Vector2)enemigo.position + moveDirection;
+
+        
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Muro"))
+        {
+            
+            moveDirection *= -1;
+            
+        }
+        
+    }
+}
